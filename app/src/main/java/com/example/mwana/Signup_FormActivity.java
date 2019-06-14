a976f7d355abf4cb9704842f9ee65779ee1f8e72
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-06-14 14:15:44 +0300

    created sign up form

--- app/src/main/java/com/example/mwana/Signup_FormActivity.java
+++ app/src/main/java/com/example/mwana/Signup_FormActivity.java
@@ -9,5 +9,6 @@ public class Signup_FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_form);
+        getSupportActionBar().setTitle("Sign Up Form");
     }
 }
