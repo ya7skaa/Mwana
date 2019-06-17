fa9f566d7150b48035cf7b152781c46740418078
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-06-17 18:22:46 +0300

    created assests folder

--- app/src/main/java/com/example/mwana/Login_Form.java
+++ app/src/main/java/com/example/mwana/Login_Form.java
@@ -8,6 +8,6 @@ public class Login_Form extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login__form);
+        setContentView(R.layout.activity_login_form);
     }
 }
