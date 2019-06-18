689d302b4dc113fcfc5d676f6818b581f79e59d3
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-06-18 15:31:25 +0300

    added on swipe listener class

---
+++ app/src/main/java/com/example/mwana/OnSwipeTouchListener.java
@@ -0,0 +1,4 @@
+package com.example.mwana;
+
+public class OnSwipeTouchListener {
+}
--- app/src/main/java/com/example/mwana/Signup_FormActivity.java
+++ app/src/main/java/com/example/mwana/Signup_FormActivity.java
@@ -1,14 +1,80 @@
 package com.example.mwana;
 
+//import android.graphics.Typeface;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.widget.TextView;
+//
+//public class Signup_FormActivity extends AppCompatActivity {
+//
+//    private TextView mAppNameTextView;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_signup_form);
+//
+//        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+//        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+//        mAppNameTextView.setTypeface(ostrichFont);
+//
+//        getSupportActionBar().setTitle("Sign Up Form");
+//    }
+//}
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Signup_FormActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup_form);
-        getSupportActionBar().setTitle("Sign Up Form");
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        imageView.setOnTouchListener(new OnSwipeTouchListener(getApplicationContext()) {
+            public void onSwipeTop() {
+            }
+
+            public void onSwipeRight() {
+                if (count == 0) {
+                    imageView.setImageResource(R.drawable.login_img);
+                    textView.setText("Night");
+                    count = 1;
+                } else {
+                    imageView.setImageResource(R.drawable.login_img);
+                    textView.setText("Morning");
+                    count = 0;
+                }
+            }
+
+            public void onSwipeLeft() {
+                if (count == 0) {
+                    imageView.setImageResource(R.drawable.login_img);
+                    textView.setText("Night");
+                    count = 1;
+                } else {
+                    imageView.setImageResource(R.drawable.login_img);
+                    textView.setText("Morning");
+                    count = 0;
+                }
+            }
+
+            public void onSwipeBottom() {
+            }
+
+        });
     }
 }
